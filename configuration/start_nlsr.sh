5d403071584f59d0accd043096dc44a5f7af9011
jdehart1 <jdd@arl.wustl.edu>
2015-08-21 14:37:00 -0500

    delete nfd output files

--- configuration/start_nlsr.sh
+++ configuration/start_nlsr.sh
@@ -2,11 +2,12 @@
 
 NAME=$1
 # remove any old restart log so we cn see how much it restarts each time.
-rm ./NLSR_OUTPUT/$NAME.restart.log
+rm ./NLSR_OUTPUT/$NAME.NLSR.restart.log
 while true
 do
   echo "starting nlsr >$NAME<"
   nlsr -f ./NLSR_CONF/$NAME.conf > ./NLSR_OUTPUT/$NAME.OUTPUT 2>&1 
-  echo "nlsr died, sleep 10 and restart" >> ./NLSR_OUTPUT/$NAME.NLSR.restart.log 
+  DATE=`date`
+  echo "${DATE}: nlsr died, sleep 10 and restart" >> ./NLSR_OUTPUT/$NAME.NLSR.restart.log 
   sleep 10
 done
