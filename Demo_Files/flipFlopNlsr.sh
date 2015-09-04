cdf6b7d61b12d272fc4caa3dbf73b99e4416a673
jdehart1 <jdd@arl.wustl.edu>
2015-09-04 17:07:29 -0500

    fix for change in costs

--- Demo_Files/flipFlopNlsr.sh
+++ Demo_Files/flipFlopNlsr.sh
@@ -8,12 +8,14 @@ do
   cp ua.conf.MOD ../configuration/NLSR_CONF/ua.conf
   cp wu.conf.MOD ../configuration/NLSR_CONF/wu.conf
 
+  echo "Modified"
   ssh $h28x1 "killall nlsr"
   ssh $h3x1  "killall nlsr"
   sleep 120 
   cp ua.conf.ORIG ../configuration/NLSR_CONF/ua.conf
   cp wu.conf.ORIG ../configuration/NLSR_CONF/wu.conf
 
+  echo "Original"
   ssh $h28x1 "killall nlsr"
   ssh $h3x1  "killall nlsr"
   sleep 120 
