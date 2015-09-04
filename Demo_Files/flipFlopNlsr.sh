8b1a84c6ea760e3a0abbbab280d7ddf242854769
jdehart1 <jdd@arl.wustl.edu>
2015-09-04 12:05:24 -0500

    Add scripts and files for controlling NLSR changes for UA and WU

---
+++ Demo_Files/flipFlopNlsr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/.topology
+
+while true
+do
+
+  cp ua.conf.MOD ../configuration/NLSR_CONF/ua.conf
+  cp wu.conf.MOD ../configuration/NLSR_CONF/wu.conf
+
+  ssh $h28x1 "killall nlsr"
+  ssh $h3x1  "killall nlsr"
+  sleep 120 
+  cp ua.conf.ORIG ../configuration/NLSR_CONF/ua.conf
+  cp wu.conf.ORIG ../configuration/NLSR_CONF/wu.conf
+
+  ssh $h28x1 "killall nlsr"
+  ssh $h3x1  "killall nlsr"
+  sleep 120 
+done
+
+
