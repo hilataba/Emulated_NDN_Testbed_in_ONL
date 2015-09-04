02d4bffa896296d08b1f540fe9d86333a5efe1d5
jdehart1 <jdd@arl.wustl.edu>
2015-09-04 12:36:02 -0500

    Fixes

--- Demo_Files/runTrafficClient.sh
+++ Demo_Files/runTrafficClient.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <VM password>"
+  exit 0
+fi
+
 source ~/.topology
 
-ssh $VMsmall29 "ndn-traffic -i 20 NDN_Traffic_Client_KISTI >& ndn-traffic.log"
+sshpass -p$1 ssh $VMsmall29 "ndn-traffic -i 20 NDN_Traffic_Client_KISTI >& ndn-traffic.log"
+
+
--- Demo_Files/runTrafficServer.sh
+++ Demo_Files/runTrafficServer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <VM password>"
+  exit 0
+fi
+
 source ~/.topology
 
-ssh $VMsmall37 "ndn-traffic -i 20 NDN_Traffic_Server_KISTI >& ndn-traffic-server.log"
+sshpass -p$1 ssh $VMsmall37 "ndn-traffic-server NDN_Traffic_Server_KISTI >& ndn-traffic-server.log"
